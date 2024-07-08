e3a21bab07cbd83c97f4eca28a186148d30daeb0
Philip <fsarg2007@gmail.com>
2024-07-08 23:22:10 +0300

    finished functions

--- pages/Delivery_list/index.js
+++ pages/Delivery_list/index.js
@@ -1,26 +1,81 @@
 import React, {useEffect, useStatem, useRef, useState} from 'react'
+import Router from 'next/router'
 import Order from './components/Order'
+import Footer from './components/Footer';
+import Header from './components/Header';
 export default function index() {
-    const [data, setData] = useState({});
-    const [positions, setPositions] = useState([]);
-    useEffect(() => {
-    fetch('http://localhost:8080/order')
-              .then((response) => response.json())  
-              .then((data) => {
-                setData(data);
-                setPositions(data.positions);
-        })
+  const [data, setData] = useState([]);
+  const [total_price, setTotal] = useState();
+  const [delivery_price, setDelivery] = useState();
+  const [to_cashier, setCashier] = useState();
+  const [style, setStyle] = useState('hidden');
+
+
+  useEffect(() => {
+  fetch('http://localhost:8080/orders')
+            .then((response) => response.json())  
+            .then((data) => {
+              setData(data.orders);
+              setTotal(data.total_price);
+              setDelivery(data.delivery_price);
+              setCashier(data.to_cashier);
+      })
   }, []);
 
+  const handleClickRefresh = async (e) => {
+    e.preventDefault()
+    Router.reload();
+  }
+
+  const handleClickResult = async (e) => {
+    e.preventDefault()
+    setStyle('m-10 flex')
+  }
+
   return (
-    <div>
-        <div className='margin-top'>
-            <h1 className="text-center font-extrabold text-white padding-top label-font">BigGeek Web-App</h1>
-        </div>  
-        <Order
-            input_data = {data}
-            positions = {positions}
+    <div className='wrapper'>
+      <header className='header'>
+        <Header
+        text = {'Назад'}
         />
+      </header>
+      <main className='main'>
+        <div className='overflow-hidden'>
+          <div className='mt-10'>
+            {data.length > 0 ? (  
+            <div>
+              {data.map(order => (
+                  <Order
+                    input_data = {order}
+                    positions = {order.positions}
+                  />
+                ))}
+            </div>
+              ) : (
+                <h1 className='text-center font-extrabold text-white padding-top label-font '>Loading...</h1>
+              )}
+          </div>
+          <div className='m-10 mb-3 flex justify-between'>
+            <button onClick={handleClickRefresh} className='text-white bg-gradient-to-r from-red-500 via-red-600 to-red-700 hover:bg-gradient-to-br focus:ring-5 focus:outline-none focus:ring-blue-800 font-medium text-center mb-2 p-3 rounded-md'>
+              СБРОС
+            </button>
+            <button onClick={handleClickResult} className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-5 focus:outline-none focus:ring-blue-800 font-medium text-center mb-2 p-3 rounded-md'>
+              Посчитать меня
+            </button>
+          </div>
+          <div className={style}>
+              <ul>
+                <li className='w-full border-b border-gray-900 text-white'>Общая сумма - {total_price}</li>
+                <li className='w-full border-b border-gray-900 text-white'>Сумма за доставки - {delivery_price}</li>
+                <li className='w-full border-b border-gray-900 text-white'>Вы должны отдать - {to_cashier}</li>
+              </ul>
+          </div>
+        </div>
+      </main>
+      <footer className='footer'>
+        <Footer/>
+      </footer>
     </div>
+
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,34 @@
 import React from 'react'
+import Footer from './Delivery_list/components/Footer'
+import Header from './Delivery_list/components/Header'
 function index() {
 
 
   return (
-    <div className='bg-slate-500'>
-      <div className='margin-top'>
-        <h1 className="text-center font-extrabold text-white padding-top label-font">BigGeek Web-App</h1>
-      </div>  
-      <div className='mt-4 justify-center flex'>
-        <a href='Delivery_list/'  type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium text-center mb-2 button">Маршрутный лист</a>
-      
-      </div>
+    <div className='wrapper'>
+      <header className='header'>
+        <Header
+          text = {'Отмена'}
+        />
+      </header>
+      <main className='main'>
+          <a href='Delivery_list/'>
+            <div className='mt-72 m-2 flex font-medium rounded-lg bg-zinc-700 border-gray-900 text-white p-3'>
+              <img className='main-icon rounded-full' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFoAAABaCAYAAAA4qEECAAAACXBIWXMAAAsTAAALEwEAmpwYAAACbElEQVR4nO3aTUtUURzH8elh4yrfQdQ2oVW9AGlhiza1a6GboLZRUS9BM2gRVJAFCmlIZESQLrKHjQQzqekgjZXmNXQap3y6M83jPy6GoHd2XX8X7v1+4ewOzNzP4tyHcxJGkhKanyGgRQEtCmhRQIsCWhTQooAWBbQooEUBLQpoUUCLAloU0KKAFgW0KKBFAS0KaFFAiwJaFNCigBYFtCigRQEtCmhRQEcd+nE6b+0v5+zKqGNzqyWLeqFAX3+7aInO5PY4dHu8IXa1XrepnwVLLrmBjNSya5vlWjyg3XLNDt5M7YD2xuXXjm/uhVfzvnn/O47cm7JipRZ96MX1ckOA8y+++eYef5QOHNob3n+IxdLR8tAP2De94ps39mPTzg19tVNPMoGM04Oz1jOZC+OSw4FO54rb2N4ycu2NY3WLdqE+3i2slUK7OanjOVoU0KKAFgW0KKCjDL36p2oXh7/b4buf7ETvjA1lflvUCwX6zNPZHS8r+7uS9m5hwzcv61bsTiprnWNLgYzuD8s2nnXjAb1SqNi+Bq/F3neN3bUOfA789bvp1kfLFyvqy44n9K9iNb5Lx3uHpWNPboaXRrZuhid7Z+w5N0MKKp6jRQEtCmhRQIviw3+UoadzBTvWs7WVdaArZVdH2coKfXP27LMvgW3Otg1m7MFETDZnnfUSxw3icIDm6P2YHKDxurHrSFgzR8L2rv503jr+HXKcX+OQIwUULyyigBYFtCigRQEtCmhRQIsCWhTQooAWBbQooEUBLQpoUUCLAloU0KKAFgW0KKBFAS0KaFFAiwJaFNCigBYFtCigRQFtGui/lghc2FNlZcgAAAAASUVORK5CYII="/>
+              <h1 className='ml-4 self-center text-center font-extrabold text-white padding-top label-font '>Маршрутный лист</h1>
+            </div>
+          </a>
+          <a href='Delivery_list/components/Dropdown'>
+            <div className='m-2 flex font-medium rounded-lg bg-zinc-700 border-gray-900 text-white p-3'>
+              <img className='main-icon rounded-full' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFoAAABaCAYAAAA4qEECAAAACXBIWXMAAAsTAAALEwEAmpwYAAACbElEQVR4nO3aTUtUURzH8elh4yrfQdQ2oVW9AGlhiza1a6GboLZRUS9BM2gRVJAFCmlIZESQLrKHjQQzqekgjZXmNXQap3y6M83jPy6GoHd2XX8X7v1+4ewOzNzP4tyHcxJGkhKanyGgRQEtCmhRQIsCWhTQooAWBbQooEUBLQpoUUCLAloU0KKAFgW0KKBFAS0KaFFAiwJaFNCigBYFtCigRQEtCmhRQEcd+nE6b+0v5+zKqGNzqyWLeqFAX3+7aInO5PY4dHu8IXa1XrepnwVLLrmBjNSya5vlWjyg3XLNDt5M7YD2xuXXjm/uhVfzvnn/O47cm7JipRZ96MX1ckOA8y+++eYef5QOHNob3n+IxdLR8tAP2De94ps39mPTzg19tVNPMoGM04Oz1jOZC+OSw4FO54rb2N4ycu2NY3WLdqE+3i2slUK7OanjOVoU0KKAFgW0KKCjDL36p2oXh7/b4buf7ETvjA1lflvUCwX6zNPZHS8r+7uS9m5hwzcv61bsTiprnWNLgYzuD8s2nnXjAb1SqNi+Bq/F3neN3bUOfA789bvp1kfLFyvqy44n9K9iNb5Lx3uHpWNPboaXRrZuhid7Z+w5N0MKKp6jRQEtCmhRQIviw3+UoadzBTvWs7WVdaArZVdH2coKfXP27LMvgW3Otg1m7MFETDZnnfUSxw3icIDm6P2YHKDxurHrSFgzR8L2rv503jr+HXKcX+OQIwUULyyigBYFtCigRQEtCmhRQIsCWhTQooAWBbQooEUBLQpoUUCLAloU0KKAFgW0KKBFAS0KaFFAiwJaFNCigBYFtCigRQFtGui/lghc2FNlZcgAAAAASUVORK5CYII="/>
+              <h1 className='ml-4 self-center text-center font-extrabold text-white padding-top label-font '>Тест</h1>
+            </div>
+          </a>
+      </main>
+      <footer className='footer'>
+        <Footer/>
+      </footer>
+
     </div>
   )
 }
