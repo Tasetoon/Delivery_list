3210317970331ee5f7176ed1410430471e1cae9f
Philip <fsarg2007@gmail.com>
2024-07-22 20:29:44 +0300

    test

--- client/pages/Delivery_list/components/test.js
+++ client/pages/Delivery_list/components/test.js
@@ -6,9 +6,9 @@ const Dropdown = () => {
                 <a href='https://yandex.ru/maps/?text=ВШЭ' className=' text-blue-500'>кликай сюды</a>
 
                 <div className=' text-white mt-10 h-auto w-auto flex flex-col'>
-                    <a href='https://t.me/+79060540581'>позовнить по тг</a>
-                    <a href='https://wa.me/+79060540581'>позовнить по ватсапп</a>
-                    <a href='tel:+79060540581'>позовнить просто</a>
+                    <a href='https://t.me/89060540581'>позовнить по тг</a>
+                    <a href='https://wa.me/89060540581'>позовнить по ватсапп</a>
+                    <a href='tel:89060540581'>позовнить просто</a>
                 </div>
             </div>
         
