c8fc66a41bf4c742458a09ffb86422f1d432496d
Philip <fsarg2007@gmail.com>
2024-07-22 19:28:38 +0300

    experimenting with phone calls types

--- client/pages/Delivery_list/components/test.js
+++ client/pages/Delivery_list/components/test.js
@@ -1,12 +1,17 @@
-'use client'
 import React, { useState } from 'react'
-
 const Dropdown = () => {
-
     return (
-        <div className=' h-10 w-auto'> 
-            <a href='https://yandex.ru/maps/?text=ВШЭ' className=' text-blue-500'>кликай сюды</a>
-        </div>
+            <div className=' h-10 w-auto'>
+                
+                <a href='https://yandex.ru/maps/?text=ВШЭ' className=' text-blue-500'>кликай сюды</a>
+
+                <div className=' text-white mt-10 h-auto w-auto flex flex-col'>
+                    <a href='https://t.me/+79163874752'>позовнить по тг</a>
+                    <a href='https://wa.me/+79163874752'>позовнить по ватсапп</a>
+                    <a href='tel:+79163874752'>позовнить просто</a>
+                </div>
+            </div>
+        
     )
 }
 
