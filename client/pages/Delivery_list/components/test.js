6899126fa88bbe0a018ff16fa15fa4d37ca6c37a
Philip <fsarg2007@gmail.com>
2024-07-27 14:37:51 +0300

    establish tg connection

--- client/pages/Delivery_list/components/test.js
+++ client/pages/Delivery_list/components/test.js
@@ -1,8 +1,30 @@
-import React, { useState } from 'react'
+import Script, { handleClientScriptLoad } from 'next/script';
+import React, { useState, useEffect } from 'react'
 const Dropdown = () => {
+    const [tg, setTg] = useState('');
+
+    useEffect(() => {
+        setTg(window.Telegram.WebApp);
+      }, [])
+
+    const onClose = () => {
+        tg.close();
+    }
+
+    const onReady = () => {
+        tg.ready();
+        console.log(tg)
+    }
+
     return (
             <div className=' h-10 w-auto'>
-                
+                <Script src='https://telegram.org/js/telegram-web-app.js' strategy='beforeInteractive'></Script>
+                <div>
+                    <button onClick={onClose}>Close</button>
+                </div>
+                <div>
+                    <button onClick={onReady}>Check</button>
+                </div>
                 <a href='https://yandex.ru/maps/?text=ВШЭ' className=' text-blue-500'>кликай сюды</a>
 
                 <div className=' text-white mt-10 h-auto w-auto flex flex-col'>
