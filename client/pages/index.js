4cf4dd5fa7207feeb5f7d88d1e9090fafb1b92bd
Philip <fsarg2007@gmail.com>
2024-07-27 20:06:34 +0300

    styling

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import Script, { handleClientScriptLoad } from 'next/script';
 function index() {
-  const [tg, setTg] = useState();
 
   useEffect(() => {
-      setTg(window.Telegram.WebApp);
+      window.Telegram.WebApp.setHeaderColor('secondary_bg_color');
+      window.Telegram.WebApp.setBackgroundColor('secondary_bg_color');
+      window.Telegram.WebApp.BackButton.hide()
     }, [])
 
   return (
@@ -18,18 +19,18 @@ function index() {
         </div>
       </header>
       <main className='main'>
-        <div className='section'>
+        <div>
 
         
           <a href='Delivery_list/'>
-            <div className='mt-10 m-2 flex font-medium rounded-lg p-3'>
-              <h1 className='text-xl'>📋</h1>
+            <div className='m-2 flex font-medium rounded-lg p-3 section'>
+              <img className='main-icon rounded-full' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFoAAABaCAYAAAA4qEECAAAACXBIWXMAAAsTAAALEwEAmpwYAAACbElEQVR4nO3aTUtUURzH8elh4yrfQdQ2oVW9AGlhiza1a6GboLZRUS9BM2gRVJAFCmlIZESQLrKHjQQzqekgjZXmNXQap3y6M83jPy6GoHd2XX8X7v1+4ewOzNzP4tyHcxJGkhKanyGgRQEtCmhRQIsCWhTQooAWBbQooEUBLQpoUUCLAloU0KKAFgW0KKBFAS0KaFFAiwJaFNCigBYFtCigRQEtCmhRQEcd+nE6b+0v5+zKqGNzqyWLeqFAX3+7aInO5PY4dHu8IXa1XrepnwVLLrmBjNSya5vlWjyg3XLNDt5M7YD2xuXXjm/uhVfzvnn/O47cm7JipRZ96MX1ckOA8y+++eYef5QOHNob3n+IxdLR8tAP2De94ps39mPTzg19tVNPMoGM04Oz1jOZC+OSw4FO54rb2N4ycu2NY3WLdqE+3i2slUK7OanjOVoU0KKAFgW0KKCjDL36p2oXh7/b4buf7ETvjA1lflvUCwX6zNPZHS8r+7uS9m5hwzcv61bsTiprnWNLgYzuD8s2nnXjAb1SqNi+Bq/F3neN3bUOfA789bvp1kfLFyvqy44n9K9iNb5Lx3uHpWNPboaXRrZuhid7Z+w5N0MKKp6jRQEtCmhRQIviw3+UoadzBTvWs7WVdaArZVdH2coKfXP27LMvgW3Otg1m7MFETDZnnfUSxw3icIDm6P2YHKDxurHrSFgzR8L2rv503jr+HXKcX+OQIwUULyyigBYFtCigRQEtCmhRQIsCWhTQooAWBbQooEUBLQpoUUCLAloU0KKAFgW0KKBFAS0KaFFAiwJaFNCigBYFtCigRQFtGui/lghc2FNlZcgAAAAASUVORK5CYII="/>
               <h1 className='ml-4 self-center text-center font-extrabold'>Маршрутный лист</h1>
             </div>
           </a>
           <a href='Delivery_list/components/test'>
-            <div className='m-2 flex font-medium rounded-lg p-3'>
-              <h1 className='text-xl'>📋</h1>
+            <div className='m-2 flex font-medium rounded-lg p-3 section'>
+              <img className='main-icon rounded-full' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFoAAABaCAYAAAA4qEECAAAACXBIWXMAAAsTAAALEwEAmpwYAAACbElEQVR4nO3aTUtUURzH8elh4yrfQdQ2oVW9AGlhiza1a6GboLZRUS9BM2gRVJAFCmlIZESQLrKHjQQzqekgjZXmNXQap3y6M83jPy6GoHd2XX8X7v1+4ewOzNzP4tyHcxJGkhKanyGgRQEtCmhRQIsCWhTQooAWBbQooEUBLQpoUUCLAloU0KKAFgW0KKBFAS0KaFFAiwJaFNCigBYFtCigRQEtCmhRQEcd+nE6b+0v5+zKqGNzqyWLeqFAX3+7aInO5PY4dHu8IXa1XrepnwVLLrmBjNSya5vlWjyg3XLNDt5M7YD2xuXXjm/uhVfzvnn/O47cm7JipRZ96MX1ckOA8y+++eYef5QOHNob3n+IxdLR8tAP2De94ps39mPTzg19tVNPMoGM04Oz1jOZC+OSw4FO54rb2N4ycu2NY3WLdqE+3i2slUK7OanjOVoU0KKAFgW0KKCjDL36p2oXh7/b4buf7ETvjA1lflvUCwX6zNPZHS8r+7uS9m5hwzcv61bsTiprnWNLgYzuD8s2nnXjAb1SqNi+Bq/F3neN3bUOfA789bvp1kfLFyvqy44n9K9iNb5Lx3uHpWNPboaXRrZuhid7Z+w5N0MKKp6jRQEtCmhRQIviw3+UoadzBTvWs7WVdaArZVdH2coKfXP27LMvgW3Otg1m7MFETDZnnfUSxw3icIDm6P2YHKDxurHrSFgzR8L2rv503jr+HXKcX+OQIwUULyyigBYFtCigRQEtCmhRQIsCWhTQooAWBbQooEUBLQpoUUCLAloU0KKAFgW0KKBFAS0KaFFAiwJaFNCigBYFtCigRQFtGui/lghc2FNlZcgAAAAASUVORK5CYII="/>
               <h1 className='ml-4 self-center text-center font-extrabold'>Тест</h1>
             </div>
           </a>
