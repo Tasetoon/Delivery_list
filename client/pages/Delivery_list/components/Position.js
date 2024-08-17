94a3ba7d010737abab07d1473500530a2ad17f0b
Philip <fsarg2007@gmail.com>
2024-08-18 01:20:52 +0300

    -

--- client/pages/Delivery_list/components/Position.js
+++ client/pages/Delivery_list/components/Position.js
@@ -25,7 +25,7 @@ export default function position(props){
           setPositionStyle('hidden');
           removeItem(`${props.order_id}/${props.id}`);
           window.dispatchEvent(new Event("storage"));
-          router.replace('?mainButtonClicked=false', { scroll: false })
+          router.replace(`?mainButtonClicked=false`)
         }
       }
       window.Telegram.WebApp.showConfirm('Вы уверены? \nВы вносите изменения после подсчета!', callback)
